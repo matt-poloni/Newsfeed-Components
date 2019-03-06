5eaa5db9c21ddf87fe1bff27d71471653b432ca8
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-06 17:46:49 -0500

    Actually completes Stretch 1, sliding menu out when clicking anywhere but the menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,4 +9,12 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => toggleMenu());
+menuButton.addEventListener('click', e => {
+  toggleMenu();
+  // STRETCH 1: Stops propagation to avoid collision w/ non-menu click events
+  e.stopPropagation();
+});
+
+// STRETCH 1: Click anywhere but menu to close it
+document.addEventListener('click', () => menu.classList.remove('menu--open'));
+menu.addEventListener('click', e => e.stopPropagation());
